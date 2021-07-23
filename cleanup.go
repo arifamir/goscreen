8d3bac9eb04dacc11642d7d86ddb5eb07de689d1
Arif Amir <arifamirk@gmail.com>
2021-07-23 23:23:35 +0200

    Utility methods

---
+++ cleanup.go
@@ -0,0 +1,13 @@
+package goscreen
+
+import "fmt"
+
+// Clear clears the screen
+func Clear() {
+	fmt.Print("\033[2J")
+}
+
+// MoveTopLeft moves the cursor to the top left position of the screen
+func MoveTopLeft() {
+	fmt.Print("\033[H")
+}
